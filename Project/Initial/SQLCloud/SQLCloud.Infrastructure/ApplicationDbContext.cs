9184b084c0999d0ebe19f708cfcf77ab0c5d5345
Julio Robles <Julio.Robles@GMFinancial.com>
2021-04-29 12:03:15 -0500

    Upload Initial Version

---
+++ Project/Initial/SQLCloud/SQLCloud.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SQLCloud.Infrastructure
+{
+    public class ApplicationDbContext
+    {
+    }
+}
---
+++ Project/Solution/SQLCloud/SQLCloud.Domain/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SQLCloud.Domain
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(1)]
+        public string Gender { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [MaxLength(320)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [MaxLength(20)]
+        [Phone]
+        public string PhoneNumber { get; set; }
+    }
+}
